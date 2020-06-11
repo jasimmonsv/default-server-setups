90ec8787dc2737e96a43832a6cf04b730e8c07ab
jasimmonsv <jasimmons@deloitte.com>
2020-06-11 15:03:53 -0500

    fix get-ssh-keys; optimize install.sh;

--- debian-server/get-ssh-keys.sh
+++ debian-server/get-ssh-keys.sh
@@ -2,10 +2,9 @@
 echo Provide GitHub/GitLab username:
 read GIT_USER
 
-curl -O https://github.com/github-$GIT_USER.keys
-curl -O https://gitlab.com/gitlab-$GIT_USER.keys
-
-mv ./github-$GIT_USER.keys ~/.ssh/authorized_keys
-cat gitlab-$GIT_USER.keys >> ~/.ssh/authorized_keys
+curl -O https://github.com/$GIT_USER.keys
+mv ./$GIT_USER.keys ~/.ssh/authorized_keys
+curl -O https://gitlab.com/$GIT_USER.keys
+cat $GIT_USER.keys >> ~/.ssh/authorized_keys
 chmod 640 ~/.ssh/authorized_keys
-rm ./*-$GIT_USER.keys
+rm ./$GIT_USER.keys
--- debian-server/install.sh
+++ debian-server/install.sh
@@ -6,8 +6,8 @@
 sudo add-apt-repository -yy universe
 
 # repository keys
-## chrome
-wget -q -O - "https://dl-ssl.google.com/linux/linux_signing_key.pub" | sudo apt-key add -
+sudo apt-get install -yy gnupg-agent \
+    curl
 ## Spideroak
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 573E3D1C51AE1B3D
 
@@ -18,21 +18,19 @@ sudo apt-get dist-upgrade -yy
 
 # Installations
 ## Essential
-sudo apt-get install -yy vim
-sudo apt-get install -yy vim-gui-common
-sudo apt-get install -yy vim-runtime
+sudo apt-get install -yy vim \
+    vim-gui-common \
+    vim-runtime
 
-sudo apt-get install -yy software-properties-common
-sudo apt-get install -yy apt-transport-https
-sudo apt-get install -yy ca-certificates
-sudo apt-get install -yy curl
-sudo apt-get install -yy sshd
-sudo apt-get install -yy ufw
-sudo apt-get install -yy gnupg-agent
-sudo apt-get install -yy git
-sudo apt-get install -yy tmux
-sudo apt-get install -yy ctags
-sudo apt-get install -yy xclip
+sudo apt-get install -yy software-properties-common \
+    apt-transport-https \
+    ca-certificates \
+    sshd \
+    ufw \
+    git \
+    tmux \
+    ctags \
+    xclip
 
 ## Programming Languages/Tools
 sudo apt-get install -yy python
@@ -40,18 +38,6 @@ sudo apt-get install -yy python3
 sudo apt-get install -yy python-dev
 sudo apt-get install -yy python-pip
 sudo apt-get install -yy python3-pip
-
-# Docker
-sudo apt-get install -yy docker-ce
-sudo apt-get install -yy docker-ce-cli
-sudo apt-get install -yy containerd.io
-
-# install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.1/contrib/completion/bash/docker-compose \
-    -o /etc/bash_completion.d/docker-compose
 
 # Cleanup
 sudo apt-get autoremove -yy
