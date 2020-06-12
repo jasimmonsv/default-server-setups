865e16c2d4253a45e8a58d136fe6eef12ae7be38
jasimmonsv <jasimmons@deloitte.com>
2020-06-11 19:51:11 -0500

    add docker daemon setup;

--- debian-server/install_docker.sh
+++ debian-server/install_docker.sh
@@ -13,3 +13,20 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
+
+# Setup docker daemon
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
