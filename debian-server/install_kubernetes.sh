b501daba4487414828e499a15c5e3cc69c784834
jasimmonsv <jasimmons@deloitte.com>
2020-06-11 20:11:55 -0500

    add kubernetes ports;

--- debian-server/install_kubernetes.sh
+++ debian-server/install_kubernetes.sh
@@ -12,4 +12,26 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install kubernets and tools
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# Configure Firewall
+echo Is this the master node? (y/n)
+read master_node
+
+if [ $master_node == 'y' ] ; then
+    sudo ufw allow 6443/tcp
+    sudo ufw allow 2379/tcp
+    sudo ufw allow 2380/tcp
+    sudo ufw allow 10250/tcp
+    sudo ufw allow 10251/tcp
+    sudo ufw allow 10252/tcp
+else
+    sudo ufw allow 10250/tcp
+    sudo ufw allow 30000-32767/tcp
+fi
